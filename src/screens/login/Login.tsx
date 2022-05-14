1d225dc59ecacd0a41007b2842adde1ba91a7cc0
abner luan <abnerluan7@gmail.com>
2022-05-14 01:21:26 -0300

    uppercase

---
+++ src/screens/login/Login.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { useAuth } from 'Providers/AuthProvider';
+
+export function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const { user, signUp, signIn } = useAuth();
+
+  useEffect(() => {
+    if (user != null) {
+      navigation.navigate('Milks');
+    }
+  }, [user]);
+
+  const onPressSignIn = async () => {
+    console.log('Trying sign in with user: ' + email);
+    try {
+      await signIn(email, password);
+    } catch (error) {
+      const errorMessage = `Failed to sign in: ${error.message}`;
+      console.error(errorMessage);
+      Alert.alert(errorMessage);
+    }
+  };
+
+  const onPressSignUp = async () => {
+    console.log('Trying Sign Up with user: ' + email);
+    try {
+      await signUp(email, password);
+      signIn(email, password);
+    } catch (error) {
+      const errorMessage = `Failed to sign up: ${error.message}`;
+      console.error(errorMessage);
+      Alert.alert(errorMessage);
+    }
+  };
+
+  return (
+    <View>
+      <Text>Sign Up or Sign In:</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={setEmail}
+          value={email}
+          placeholder="email"
+          style={styles.inputStyle}
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          placeholder="password"
+          style={styles.inputStyle}
+          secureTextEntry
+        />
+      </View>
+      <Button onPress={onPressSignIn} title="Sign In" />
+      <Button onPress={onPressSignUp} title="Sign Up" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 5,
+  },
+  inputStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 2,
+  },
+});
